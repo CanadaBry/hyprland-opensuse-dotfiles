f8df034f02d18c56f956b6b48874b130b56737dd
bry <bry@bry-h1.localdomain>
2024-10-26 01:05:00 -0400

    Fixed a few bugs in setup.sh

--- setup.sh
+++ setup.sh
@@ -34,9 +34,9 @@ sudo zypper install git docker neovim steam gamescope tree
 
 # Install flatpaks (Using flathub)
 sudo zypper install flatpak
-flatpak install com.spotify.Client # Spotify
-flatpak install dev.vencord.Vesktop # 3rd party Discord client with screenshare+audio
-flatpak install io.gitlab.librewolf-community # Privacy focused Firefox browser
+sudo flatpak install com.spotify.Client # Spotify
+sudo flatpak install dev.vencord.Vesktop # 3rd party Discord client with screenshare+audio
+sudo flatpak install io.gitlab.librewolf-community # Privacy focused Firefox browser
 
 #####################
 ### Configuration ###
@@ -47,7 +47,7 @@ mkdir -p ~/Pictures/Wallpapers/
 cp ./wallpaper/opensuse-uw.jpg ~/Pictures/Wallpapers/
 
 # Copy configs
-cp -r ./config ~/.config
+cp -r ./config/* ~/.config
 
 # Copy greetd config and start script
 sudo sed "s/YOUR_USER/$USER/g" ./root/etc/greetd/config.toml > /etc/greetd/config.toml # Specify current user as auto-login target
@@ -58,8 +58,8 @@ sudo chmod +x /usr/local/bin/start-hyprland # Just in case
 sudo systemctl set-default graphical.target
 
 # Enable services
-sudo systemctl enable --now greetd # Login Manager
-systemctl enable --user --now pipewire pipewire-pulse wireplumber # Audio services
+sudo systemctl enable greetd # Login Manager
+systemctl enable --user pipewire pipewire-pulse wireplumber # Audio services
 
 # Set LibreWolf as default browser
 xdg-mime default io.gitlab.librewolf-community.desktop x-scheme-handler/http
